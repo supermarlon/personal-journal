80ec0f3624d7bfa0c6a26f4646cb273a78263b7e
supermarlon <supercoolermarlon@gmail.com>
2023-02-06 12:01:00 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,11 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next journal entry bio in seconds."
           />
-          <meta property="og:site_name" content="twitterbio.com" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
-          />
-          <meta property="og:title" content="Twitter Bio Generator" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
-          <meta
-            name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next journal entry in seconds."
           />
           <meta
             property="og:image"
